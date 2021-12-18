8f29bb80350f22b224760231dbc4c7b321c7d6e1
Rafael Matos <rafaelmatosqa@gmail.com>
2021-12-18 13:47:57 -0300

    criando projeto, configurando e criando o primeiro teste

---
+++ cypress/integration/exemplo_teste.js
@@ -0,0 +1,11 @@
+
+
+describe('Teste Cypress com Docker', ()=> {
+    it('Deve acessar o site', ()=> {
+        cy.visit('https://www.example.com')
+    })
+
+    it('Deve visualizar elemento h1', ()=> {
+        cy.get('h1').should('be.visible')
+    })
+})
